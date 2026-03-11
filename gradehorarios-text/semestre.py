7d95902531c47c1d2202c5080c61232ffdac90b1
Miguel Ângelo <miguel.angelo11@aluno.ifce.edu.br>
2026-03-11 09:12:14 -0300

    Correção semestre

--- gradehorarios-text/semestre.py
+++ gradehorarios-text/semestre.py
@@ -1,6 +1,6 @@
 class Semestre:
 
-    def __init__(self, id, semestre_letivo, disciplinas):
+    def __init__(self, id = int, semestre_letivo = int, disciplinas = list):
         self.id = id
         self.semestre_letivo = semestre_letivo
         # self.disciplinas = Disciplina 
