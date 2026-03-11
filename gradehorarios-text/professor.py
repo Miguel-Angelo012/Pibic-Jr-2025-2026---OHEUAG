6155ee1aeeac8ae975b0b71965fc66599f8f7a62
Thomaz Maia <thomaz.maia@ifce.edu.br>
2026-03-11 09:12:29 -0300

    Add tipos

--- gradehorarios-text/professor.py
+++ gradehorarios-text/professor.py
@@ -1,6 +1,6 @@
 class Professor:
 
-    def __init__(self, matricula, nome, dias_concentrados):
+    def __init__(self, matricula : int, nome : string, disciplinas : list, horarios : list, horarios_bloqueados : list, dias_concentrados : bool):
         self.matricula = matricula
         self.nome = nome
         # self.disciplina = Disciplina
