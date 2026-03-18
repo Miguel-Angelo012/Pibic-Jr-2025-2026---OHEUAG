019cec844341fb19a0f40c5b85b6e1ae7b42b56a
Miguel Ângelo <miguel.angelo11@aluno.ifce.edu.br>
2026-03-18 14:12:14 -0300

    alterações Main.py

--- gradehorarios-text/main.py
+++ gradehorarios-text/main.py
@@ -14,5 +14,12 @@ print(prof.getHorarios_Bloqueados())
 matematica1 = Disciplina("MAT1", 1, "Matemática 1", 40, False, sala)
 matematica1.addHorario("SEG-M-AB")
 matematica1.addProfessor(prof)
+matematica1.addAluno("Miguel")
+matematica1.addAluno("Valdemir")
+matematica1.addAluno("Pedro")
 
 print(matematica1)
+
+Miguel = Aluno(20250001, "Miguel", Semestre(1, 2025))
+
+print(Miguel.nome)
