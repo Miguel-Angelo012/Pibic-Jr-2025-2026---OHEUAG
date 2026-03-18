ccdc437b6160b886c69b3c39999de763ac1ec3a5
Miguel Ângelo <miguel.angelo11@aluno.ifce.edu.br>
2026-03-18 14:53:30 -0300

    Remoção da função "getDisciplina" e alteração nos parâmetros
    tentativa de adicionar "id_semestre" a partir da classe Semestre e não de parâmetros

--- gradehorarios-text/aluno.py
+++ gradehorarios-text/aluno.py
@@ -5,13 +5,8 @@ class Aluno:
     def __init__(self,
                  matricula = int,
                  nome = str,
-                 id_semestre = Semestre):
+                 ):
 
         self.matricula = matricula
         self.nome = nome
-        self.id_semestre = id_semestre
-        
-    def getDisciplina(self):
-        from disciplina import Disciplina
-        
-
+        self.id_semestre = Semestre.getSemestreLetivo()
