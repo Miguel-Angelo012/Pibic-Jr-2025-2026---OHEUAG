44e2a95b6996ae71ee2f145bd5142a9fead181b9
Miguel Ângelo <miguel.angelo11@aluno.ifce.edu.br>
2026-03-18 14:55:32 -0300

    alteração no main.py
    adição de alunos, sala, professores e texts caso não fossem adicionados os parâmetros de "matemática1"

--- gradehorarios-text/main.py
+++ gradehorarios-text/main.py
@@ -4,22 +4,23 @@ from professor import Professor
 from sala import Sala
 from semestre import Semestre
 
-sala = Sala(10, 30, False)
+sala = Sala(10, 30, True)
+
 prof = Professor(1058981, "Thomaz Maia")
 prof.setHorarios_Bloqueados("SEG-M-CD")
 
+prof2 = Professor(1058982, "Maria Silva")
+prof2.setHorarios_Bloqueados("SEG-M-AB")
 print(prof.getHorarios_Bloqueados())
 
 
-matematica1 = Disciplina("MAT1", 1, "Matemática 1", 40, False, sala)
+matematica1 = Disciplina("MAT1", 1, "Matemática 1", 40, False)
 matematica1.addHorario("SEG-M-AB")
+matematica1.addProfessor(prof2)
 matematica1.addProfessor(prof)
+matematica1.addSala(sala)
 matematica1.addAluno("Miguel")
 matematica1.addAluno("Valdemir")
 matematica1.addAluno("Pedro")
 
 print(matematica1)
-
-Miguel = Aluno(20250001, "Miguel", Semestre(1, 2025))
-
-print(Miguel.nome)
