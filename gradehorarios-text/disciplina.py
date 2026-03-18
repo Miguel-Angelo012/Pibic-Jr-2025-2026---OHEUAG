371d38f99eaeb54cf21979179cc937587b6c8011
Miguel Ângelo <miguel.angelo11@aluno.ifce.edu.br>
2026-03-18 14:50:34 -0300

    adição de novos métodos e verificações
    remoção de "sala" como parâmetro inicial e Adição do método "addSala", que vai verificar se a sala é compatível com a matéria; prevenção de adição a um outro professor a uma matéria já com professor; prevenção de ERRO ao printar a matéria sem: professor, alunos, horários ou sala definidos.

--- gradehorarios-text/disciplina.py
+++ gradehorarios-text/disciplina.py
@@ -10,43 +10,77 @@ class Disciplina:
                  semestre : int, 
                  nome : str, 
                  carga_horaria : int, 
-                 e_tecnica : bool, 
-                 sala : Sala):
+                 e_tecnica : bool):
         
+        #Definidos por parâmetros
         self.cod = cod
         self.semestre = semestre
         self.nome = nome
         self.carga_horaria = carga_horaria
         self.e_tecnica = e_tecnica    
-        self.sala = sala        
+
+        #Definidos por função
         self.horarios = list()        
         self.pre_requisitos = list()
-        self.professor = None 
+        self.professor = None
         self.alunos = list()
+        self.sala = None
         
     def addHorario(self, horarios : list):
         self.horarios.append(horarios)
-        # print(f"Horários adicionados à disciplina {self.nome}")
+        print(f"Horários adicionados à disciplina {self.nome}")
+
 
     def addAluno(self, aluno : Aluno):
         self.alunos.append(aluno)
 
+
     def addProfessor(self, professor : Professor):
         
+        #Prevenção caso o professor tenha um horário bloqueado que coincida com o horário da disciplina
         for item in professor.getHorarios_Bloqueados():
             if item in self.horarios:
                 print("O professor não pode ser adicionado neste horário!")
                 return
 
-        self.professor = professor
+        #Prevençao caso a disciplina já tenha um professor atribuído
+        if self.professor is not None:
+            print("A disciplina já tem um professor atribuído!")
+            return
+
+        self.professor = professor.nome
+
+
+    def addSala(self, sala : Sala):
+
+        if sala.laboratorio != self.e_tecnica:
+            print(f"Esta sala não é adequada para {self.nome}")
+            return
+
+        self.sala = sala.num_sala
+
 
     def __repr__(self):
-        # Isso aqui vai ser chamado quando tu fizer "print(nome_da_disciplina)"
+        #Caso a disciplina não tenha um professor atribuído.
+        if self.professor is None:
+            self.professor = "Sem professor"
         
+        if not self.alunos:
+            self.alunos = "Sem alunos"
+
+        if not self.horarios:
+            self.horarios = "Sem horários"
+
+        if self.sala is None:
+            self.sala = "Sem sala"
+
+        # Isso aqui vai ser chamado quando tu fizer "print(nome_da_disciplina)"
         desc = f"""Disciplina: {self.nome} \n
-        Professor: {self.professor.nome} \n
+        Professor: {self.professor} \n
         Carga Horária: {self.carga_horaria}h \n
         Horários: {self.horarios} \n
-        Sala: {self.sala.num_sala}"""
+        Sala: {self.sala} \n
+        Alunos: {self.alunos} \n
+        """
         
         return desc
