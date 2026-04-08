e4d778dec3ddb26f5174e8a3489c4cf6dc5970d5
Miguel Ângelo <miguel.angelo11@aluno.ifce.edu.br>
2026-04-07 23:51:04 -0300

    criação de funções
    verifica se a sala está ou não ocupada em tal horário

--- gradehorarios-text/Sala.py
+++ gradehorarios-text/Sala.py
@@ -9,6 +9,13 @@ class Sala:
         self.num_sala = num_sala
         self.capacidade = capacidade
         self.laboratorio = laboratorio
+        self.ocupacao = {}
+
+    def esta_ocupada(self, dia, turno, slot):
+        return self.ocupacao.get((dia, turno, slot), False)
+
+    def ocupar(self, dia, turno, slot):
+        self.ocupacao[(dia, turno, slot)] = True
 
     def __repr__(self):
         return f"Sala {self.num_sala}"
