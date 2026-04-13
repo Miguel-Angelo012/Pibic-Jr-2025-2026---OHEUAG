6dcc395ce50e3b8ba1c6088bc4eae649a38e0683
Miguel Ângelo <miguel.angelo11@aluno.ifce.edu.br>
2026-04-13 11:03:32 -0300

    Criação de funções auxiliares, inicialização da função fitness
    foi criado as seguintes funções: 'check_salas' para diminuir código e para ser utilizada na função fitness.
    
    foi criado também mais duas restrições rígidas: 
    - check_noite_manha: impede de um professor que teve aula de noite no dia anterior de ter aula no dia seguinte no ho´rario AB manhã.
    - check_aulas_no_dia: impede de um professor ter mais de 6 aulas por dia.

--- gradehorarios-text/Horario.py
+++ gradehorarios-text/Horario.py
@@ -11,6 +11,12 @@ TURNOS = {
     "T" : ["AB", "CD"],
     "N" : ["AB", "CD"],
 }
+ 
+# Restrições rígidas que faltam:
+# 5. O horário do professor deve ser alocado concentrado ou não, conforme sua escolha dentro dos 4 dias restantes;
+# 7. O horário de cada professor deve começar com pelo menos 20h livres;
+# 8. O horário também deve atender disciplinas a serem eu cursadas por alunos concludentes;
+# 10. As aulas de uma turma devem ficar concentradas numa mesma sala de aula;
 
 class Horario:
 # horario.grade["SEG"]["M"]["AB"] -> retorna um objeto do tipo Aula
@@ -77,19 +83,18 @@ class Horario:
                 continue
 
             # Filtra salas compativeis com o tipo da disciplina (lab ou nao)
-            salas_compativeis = []
-            for sala in self.salas:
-                
-                # Verifica se a sala já está ocupada naquele horário
-                if sala.esta_ocupada(dia, turno, slot):
-                    print(f"Sala {sala} já está ocupada")
-                    continue
+            salas_compativeis = self.check_salas(dia, turno, slot, disciplina)
 
-                if sala.laboratorio == disciplina.e_tecnica:
-                    salas_compativeis.append(sala)
- 
             if len(salas_compativeis) == 0:
-                print(f"Aviso: nenhuma sala compativel para {disciplina.nome}.")
+                continue
+            
+            # Verifica se ontem teve aula a noite para bloquear o slot M-AB 
+            dia_atual = DIAS.index(dia)
+            if self.check_noite_manha(dia_atual, turno, slot):
+                continue
+
+            # Verifica se o professor já possui 6 aulas no dia atual
+            if self.check_aulas_no_dia(dia) >= 6:
                 continue
 
             sala = random.choice(salas_compativeis)
@@ -97,16 +102,76 @@ class Horario:
             disciplina.addProfessor(self.professor) # Atribuiu o professor a disciplina
             sala.ocupar(dia, turno, slot) # Ocupa aquela sala naquele horário
     
+    def funcao_fitness(self):
+        pontuacao = 0
+        
+        for aula in self.grade:
+            pass
+        # for dia in DIAS:
+        #     for turno in TURNOS:
+        #         for slot in TURNOS[turno]:
+                    
+        #             if 
+
     def horario_grade(self, dia, turno, slot):  # retorna um objeto do tipo Aula
         
         aula = self.grade[dia][turno][slot]
         if aula is None:
-            print("Slot vazio")
+            print("Slot vazio\n")
         else:
             if aula.sala.laboratorio == True:
-                print(f"Matéria: {aula.disciplina.nome}\nTurma: {aula.turma.cod}\nLaboratório: {aula.sala.num_sala}")
+                print(f"Matéria: {aula.disciplina.nome}\nTurma: {aula.turma.cod}\nLaboratório: {aula.sala.num_sala}\n")
             else:
-                print(f"Matéria: {aula.disciplina.nome}\nTurma: {aula.turma.cod}\nSala: {aula.sala.num_sala}")
+                print(f"Matéria: {aula.disciplina.nome}\nTurma: {aula.turma.cod}\nSala: {aula.sala.num_sala}\n")
+    
+    # Funções Auxiliares:
+
+    def check_salas(self, dia, turno, slot, disciplina):
+        salas_compativeis = []
+
+        for sala in self.salas:
+
+            # sala ocupada → ignora
+            if sala.esta_ocupada(dia, turno, slot):
+                continue
+
+            # compatibilidade lab / não lab
+            if sala.laboratorio == disciplina.e_tecnica:
+                salas_compativeis.append(sala)
+
+        return salas_compativeis
+
+    def check_noite_manha(self, dia_atual, turno, slot):
+        # Verifica se há aula de noite no dia anterior
+        if turno == "M" and slot == "AB":
+            if dia_atual == 0:
+                pass
+            else:
+                dia_anterior = DIAS[dia_atual - 1]
+                
+                for slot in TURNOS["N"]:
+                    if self.grade[dia_anterior]['N'][slot] is not None:
+                        return True
+        
+        # Verifica se há aula de manhã no horário AB no próximo dia
+        if turno == "N":
+            if dia_atual < len(DIAS) - 1:
+                dia_seguinte = DIAS[dia_atual + 1]
+
+                # verifica se já existe M-AB no dia seguinte
+                if self.grade[dia_seguinte]["M"]["AB"] is not None:
+                    return True
+        return False
+    
+    def check_aulas_no_dia(self, dia):
+        total = 0
+
+        for turno in TURNOS:
+            for slot in TURNOS[turno]:
+                if self.grade[dia][turno][slot] is not None:
+                    total += 2  # cada slot vale 2 aulas
+
+        return total
     
     def __repr__(self):
         # Cabecalho
